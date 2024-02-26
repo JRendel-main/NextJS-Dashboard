5f18bf57b6d9cf818ab5fc0894775e72dc69d58d
JRendel <mrskybrine@gmail.com>
2024-02-26 23:04:51 +0800

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from 'next/font/google'
+import { Lustria } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin']})
+export const lusitana = Lustria({subsets: ['latin'], weight: ['400']})
